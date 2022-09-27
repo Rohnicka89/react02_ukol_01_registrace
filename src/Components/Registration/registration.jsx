343e63c29878d0b0390018da6a02c8a446b4516c
Beitlerová Aneta <Beitlerova@MND.CZ>
2022-09-27 10:15:12 +0200

    Oprava dle komentářů Lenky

--- src/Components/Registration/registration.jsx
+++ src/Components/Registration/registration.jsx
@@ -7,11 +7,18 @@ const Registration = () => {
 
     const handleSubmit = (event)=>{
         event.preventDefault();
-        if(user.password === user.passwordConfirm){
+        if(user.password === user.passwordConfirm && user.password != ''){
             console.log(user)
             alert('Užvivatel '+user.username+' byl zaregistrován s e-amilovou adresou: '+user.email)
-        }else alert('Hesla nesouhlasí.')
+        }else alert('Špatně vyplněný formulář. Není vyplněný e-mail nebo nesouhlasí hesla.')
     }
+
+    const MailCheck = (e)=>{
+        setUser({...user,email:e.target.value})
+        if(user.email.includes('@') && user.username === ''){
+            setUser({...user,username: user.email.substring(0,user.email.indexOf('@'))})
+        }}
+
 
     const [user,setUser] = useState({
         username: '',
@@ -25,12 +32,7 @@ const Registration = () => {
         <form onSubmit={handleSubmit} className='form'>
             <label>
                 E-mail Adress: 
-                <input  onChange={(e)=>{
-                                    setUser({...user,email:e.target.value})
-                                    if(user.email.includes('@') && user.username === ''){
-                                        setUser({...user,username: user.email.substring(0,user.email.indexOf('@'))})
-                                    }
-                                    }}/>
+                <input  onChange={MailCheck}/>
             </label>
             <label>
                 User name: 
@@ -38,11 +40,11 @@ const Registration = () => {
             </label>
             <label>
                 Password: 
-                <input onChange={(e)=>{setUser({...user,password:e.target.value})}} value={user.password}/>
+                <input onChange={(e)=>{setUser({...user,password:e.target.value})}} value={user.password} type="password"/>
             </label>
             <label>
                 Confirm password: 
-                <input onChange={(e)=>{setUser({...user,passwordConfirm:e.target.value})}} value={user.passwordConfirm}/>
+                <input onChange={(e)=>{setUser({...user,passwordConfirm:e.target.value})}} value={user.passwordConfirm} type="password"/>
             </label>
             <button onSubmit={handleSubmit} type="submit">REGISTER</button>
         </form>
